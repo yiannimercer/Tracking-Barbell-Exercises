05f7511ec821901537227bedb0c08226cf88545a
yiannimercer <YJMercer@gmail.com>
2022-12-19 22:19:52 -0600

    make_dataset script template

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,49 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from glob import glob
+
+# --------------------------------------------------------------
+# Read single CSV file
+# --------------------------------------------------------------
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+# --------------------------------------------------------------
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
 
-    main()
+
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
